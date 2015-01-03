70214a6bd7d2f7a7da7ceb9f732302a302c9967e
schedul-xor <schedul.xor@gmail.com>
2015-01-03 18:41:59 +0900

    Tried building binary QT

--- src/schedul/qt/nodestatus.js
+++ src/schedul/qt/nodestatus.js
@@ -1,12 +1,14 @@
 goog.provide('schedul.qt.NodeStatus');
 
 
+
 /**
  * @enum {!number}
  */
 schedul.qt.NodeStatus = {
-  IS_MYSTERIOUS: 0,
-  IS_SURELY_LEAF: 1,
-  IS_SURELY_BRANCH: 2,
-  HAS_MORE_DEEPER_STATUSES: 3
+  ROOT:0,
+  EXISTING_TERMINAL:1,
+  EMPTY_TERMINAL:2,
+  SURELY_MIXED:3,
+  GRAY:4
 };
--- test/schedul/qt/smarttreenode.test.js
+++
@@ -1,2 +0,0 @@
-describe('schedul.qt.SmartTreeNode', function() {
-});
